47bd9e3f22211d0529b889bd745b98b7219fe6d6
Daniel Rodriguez <maieutiquer@gmail.com>
2024-07-19 16:55:32 +0300

    add more methods for linked list, add eslint

--- data-structures/linked-list/singly-linked-list.js
+++ data-structures/linked-list/singly-linked-list.js
@@ -1,7 +1,7 @@
-const singlyLinkedListSample2 = { data: 42, next: null }
-const singlyLinkedListSample1 = { data: 422, next: singlyLinkedListSample2 }
+// const singlyLinkedListSample2 = { data: 42, next: null }
+// const singlyLinkedListSample1 = { data: 422, next: singlyLinkedListSample2 }
 
-// const createEmpty = () => ({ data: null, next: null }) // are empty lists needed?
+export const createEmpty = () => ({ data: null, next: null }) // are empty lists needed?
 export const createFromValue = (value) => ({ data: value, next: null })
 export const peakFirst = ({ data }) => data // is this function redundant?
 
@@ -9,12 +9,35 @@ export const peakLast = ({ data, next }) =>
   next === null ? data : peakLast(next)
 // const peakAtIndex = () => {}
 // const findFirstIndexOf = () => {}
-// const findLastIndexOf = () => {}
+// const findLastIndexOf = () => {} // use reverse()
 // const insertAfterIndex = () => {}
 export const addFirst = (value, handle) => ({ data: value, next: handle })
 export const addLast = (value, { data, next }) =>
   next === null ? { data, next: createFromValue(value) } : addLast(value, next)
-// const removeFirst = () => {}
-// const removeLast = () => {}
+export const removeFirst = ({ next }) => next
+// const removeLast = ({data, next}) => next === null ?  ({data, next:null}) :
 // const merge = () => {}
 // const reverse = () => {}
+export const toArray = ({ data, next }) =>
+  next === null ? [data] : [data, ...toArray(next)]
+export const fromArray = (array) =>
+  array
+    .toReversed()
+    .slice(1)
+    .reduce((acc, value) => addFirst(value, acc), createFromValue(array.at(-1)))
+// const slice = () => {}
+// const forEach = () => {}
+// const reduce = () => {}
+// const reverse = () => {}
+// const sort = () => {}
+// const checkIsSorted = () => {}
+// const zip = () => {}
+// const cartesianProduct = () => {}
+// const adjacentDifference = () => {}
+// const adjacentFind = () => {} // use predicate
+export const count = ({ next }) => 1 + (next === null ? 0 : count(next))
+
+export const map = (callbackFn, { data, next }) => ({
+  data: callbackFn(data),
+  next: next === null ? null : map(callbackFn, next),
+})
--- data-structures/linked-list/singly-linked-list.test.js
+++ data-structures/linked-list/singly-linked-list.test.js
@@ -1,17 +1,30 @@
 import {
+  fromArray,
+  toArray,
   createFromValue,
   peakFirst,
   peakLast,
   addFirst,
+  removeFirst,
   addLast,
+  map,
 } from "./singly-linked-list.js"
 
 describe("singly linked list", () => {
+  test("from array and to array", () => {
+    const initialArray = [3, 6, 10, 11, 7]
+    const linkedListFromArray = fromArray(initialArray)
+    const arrayFromLinkedList = toArray(linkedListFromArray)
+
+    expect(arrayFromLinkedList).toStrictEqual(initialArray)
+    expect(count).toBe(5)
+  })
   test("create from value and peak first", () => {
     const initialValue = 42
     const createdList = createFromValue(initialValue)
     const peakedValue = peakFirst(createdList)
     expect(peakedValue).toBe(initialValue)
+    expect(count).toBe(1)
   })
   test("create from value, add first and peak first", () => {
     const initialValue = 42
@@ -20,6 +33,8 @@ describe("singly linked list", () => {
     const listWithAddedValue = addFirst(valueToAdd, createdList)
     const peakedValue = peakFirst(listWithAddedValue)
     expect(peakedValue).toBe(valueToAdd)
+
+    expect(count).toBe(2)
   })
   test("create from value, add last once and peak last", () => {
     const initialValue = 42
@@ -40,5 +55,37 @@ describe("singly linked list", () => {
     )
     const peakedValue = peakLast(listWithAddedValueFinal)
     expect(peakedValue).toBe(valueToAdd)
+  })
+  test("create from value, add first and remove first", () => {
+    const initialValue = 42
+    const createdList = createFromValue(initialValue)
+    const listWithAddedValueIntermediate = addFirst(111, createdList)
+    const listWithAddedValueFinal = removeFirst(listWithAddedValueIntermediate)
+    const peakedValue = peakLast(listWithAddedValueFinal)
+    expect(peakedValue).toBe(initialValue)
+  })
+  test("create from value, add last and remove first", () => {
+    const createdList = createFromValue(111)
+    const valueToAdd = 42
+    const listWithAddedValueIntermediate = addLast(valueToAdd, createdList)
+    const listWithAddedValueFinal = removeFirst(listWithAddedValueIntermediate)
+    const peakedValue = peakLast(listWithAddedValueFinal)
+    expect(peakedValue).toBe(valueToAdd)
+  })
+})
+
+describe("map", () => {
+  // test.skip("compare with js array map", () => {})
+  test("create from value, map", () => {
+    const initialValue = 2
+    const createdList = createFromValue(initialValue)
+    const valueToAdd = 3
+    const listWithAddedValue = addLast(valueToAdd, createdList)
+    // console.log(toArray(listWithAddedValue))
+    const mappedList = map((x) => x * 3, listWithAddedValue)
+    const peakedFirstValue = peakFirst(mappedList)
+    const peakedLastValue = peakLast(mappedList)
+    expect(peakedFirstValue).toBe(6)
+    expect(peakedLastValue).toBe(9)
   })
 })
