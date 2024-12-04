2d5980372a7315a3d6c7a84242db5e9c0d8173e3
Daniel Rodriguez <maieutiquer@gmail.com>
2024-12-04 02:07:17 +0200

    aoc2024 d03 p2

--- aoc-2024/03.js
+++ aoc-2024/03.js
@@ -2,3 +2,18 @@ export const p1 = (input) =>
   [...input.matchAll(/mul\(([0-9]{1,3}),([0-9]{1,3})\)/dg)]
     .map((match) => match[1] * match[2])
     .reduce((a, b) => a + b, 0)
+
+export const p2 = (input) =>
+  input
+    .split("don't()")
+    .map((currentInstructions, index) =>
+      index === 0
+        ? currentInstructions
+        : currentInstructions.split("do()").slice(1).join(""),
+    )
+    .flatMap((instructions) =>
+      [...instructions.matchAll(/mul\(([0-9]{1,3}),([0-9]{1,3})\)/dg)].map(
+        (match) => match[1] * match[2],
+      ),
+    )
+    .reduce((a, b) => a + b, 0)
--- aoc-2024/03.test.js
+++ aoc-2024/03.test.js
@@ -1,4 +1,4 @@
-import { p1 } from "./03"
+import { p1, p2 } from "./03"
 import { input } from "./03.input"
 
 test.each([
@@ -6,7 +6,17 @@ test.each([
     input: `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))`,
     output: 161,
   },
-  { input: input, output: -1 },
+  { input: input, output: 196826776 },
 ])("p1", ({ input, output }) => {
   expect(p1(input)).toBe(output)
+})
+
+test.each([
+  {
+    input: `xmul(2,4)mul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))`,
+    output: 56,
+  },
+  { input: input, output: -1 },
+])("p2", ({ input, output }) => {
+  expect(p2(input)).toBe(output)
 })
